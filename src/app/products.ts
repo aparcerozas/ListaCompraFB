6d63d1ce5f89e38be546d4882141907dc4ac4e2f
aparcerozas <aparcerozas@danielcastelao.org>
2020-03-21 00:05:41 +0100

    first commit

---
+++ src/app/products.ts
@@ -0,0 +1,26 @@
+export const products = [
+  {
+    name: 'Nintendo Switch',
+    price: 300,
+    description: 'Consola híbrida de Nintendo',
+    comprado: false
+  },
+  {
+    name: 'PS4 Pro',
+    price: 400,
+    description: 'Consola de sobremesa de Sony',
+    comprado: false
+  },
+  {
+    name: 'XBOX One X',
+    price: 350,
+    description: 'Consola de sobremesa de Microsoft',
+    comprado: false
+  },
+  {
+    name: 'Google Stadia',
+    price: 100,
+    description: 'No compres esto',
+    comprado: false
+  }
+];
